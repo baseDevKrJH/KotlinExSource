23a7386698b300d2cfd868eb6b5225476a81aa12
gitTest <jihun7056@gmail.com>
2019-12-14 23:15:31 +0900

    kotlin 7 Exsource

--- src/NyetHack/Tavern.kt
+++ src/NyetHack/Tavern.kt
@@ -1,21 +1,50 @@
 package NyetHack
 
-fun main(args: Array<String>) {
-/*
-    var signatureDrink = "맥주"
-    signatureDrink = null
-*/
-/*
-    var beverage = readLine()?.capitalize()
-*/
-    var beverage = readLine()?.let{
-        if(it.isNotBlank()) {
-            it.capitalize()
-        } else {
-            "맥주"
-        }
-    }
-//    beverage = null
+const val TAVERN_NAME = "Taernyl's Folly"
 
-    println(beverage)
+fun main(args: Array<String>) {
+    //placeOrder()
+    //placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
+}
+
+private fun placeOrder() {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
+}
+
+private fun toDragonSpeak(phrase: String) =
+        phrase.replace(Regex("[aeiou]")) {
+            when(it.value) {
+                "a" -> "4"
+                "e" -> "3"
+                "i" -> "1"
+                "o" -> "0"
+                "u" -> "|_|"
+                else -> it.value
+            }
+        }
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
+
+    val data = menuData.split(',')
+    val type = data[0]
+    val name = data[1]
+    var price = data[2]
+    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+    println(message)
+
+//    var phrase = "와, $name 진짜 좋구나!"
+//    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
+
+    val phrase = if (name == "Dragon's Breath") {
+        "마드리갈이 감탄한다: ${toDragonSpeak("와, $name 진짜 좋구나!")}"
+    } else {
+        "마드리갈이 말한다: 감사합니다 $name."
+    }
+    println(phrase)
 }
